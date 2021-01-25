a8f405b8786eaa1a2f16570893022955c87b49e3
connorourke <conn.orourke@gmail.com>
2021-01-25 13:47:18 +0000

    Update .coveragerc

--- crystal_torture/tort.py
+++ crystal_torture/tort.py
@@ -1,8 +1,7 @@
 from __future__ import print_function, absolute_import, division
-import tort
+from crystal_torture import _tort
 import f90wrap.runtime
 import logging
-
 
 class Tort_Mod(f90wrap.runtime.FortranModule):
     """
